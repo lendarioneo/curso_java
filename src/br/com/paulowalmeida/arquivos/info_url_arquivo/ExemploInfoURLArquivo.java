eddf1ed91e7f44fa99bc44fbc59a8913c03f349c
pweskley25 <pauloweskley.dev@gmail.com>
2020-03-01 16:53:19 -0300

    Exemplos de leitura de diretórios e arquivos utilizando a classe File.

---
+++ src/br/com/paulowalmeida/arquivos/info_url_arquivo/ExemploInfoURLArquivo.java
@@ -0,0 +1,16 @@
+package br.com.paulowalmeida.arquivos.info_url_arquivo;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ExemploInfoURLArquivo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o caminho do arquivo: ");
+        String strPath = scanner.nextLine();
+        File path = new File(strPath);
+        System.out.println("Nome Arquivo: " + path.getName());
+        System.out.println("Caminho: " + path.getParent());
+        System.out.println("Caminho absoluto: " + path.getPath());
+    }
+}
