3937cb8bf492ac5894148d93905c1b51088d5e93
pweskley25 <pauloweskley.dev@gmail.com>
2020-03-01 16:44:11 -0300

    Exemplos de criação de diretórios e arquivos utilizando a classe File.

---
+++ src/br/com/paulowalmeida/arquivos/manipulando_pastas_file/ExemploFilePastas.java
@@ -0,0 +1,27 @@
+package br.com.paulowalmeida.arquivos.manipulando_pastas_file;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ExemploFilePastas {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite o diretório: ");
+        String strPath = scan.nextLine();
+        File path = new File(strPath);
+        File[] folders = path.listFiles(File :: isDirectory);
+        System.out.println("Pastas: ");
+        for (File folder : folders) {
+            System.out.println(folder);
+        }
+
+        File[] files = path.listFiles(File:: isFile);
+        System.out.println("Arquivos: ");
+        for (File file : files) {
+            System.out.println(file);
+        }
+
+        boolean sucesso = new File(strPath + "\\tempJava").mkdir();
+        System.out.println("Criou com sucesso o novo diretorio? " + sucesso);
+    }
+}
